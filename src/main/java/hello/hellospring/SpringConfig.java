8e1fae3d5330e1bae8026fb79c03564cef0b1097
hcjeon <hcjeon@globalorder.co.kr>
2023-01-08 16:33:08 +0900

    자바 코드로 직접 스프링 빈 등록하기

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,25 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 자바 코드로 빈 등록하기
+ */
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
