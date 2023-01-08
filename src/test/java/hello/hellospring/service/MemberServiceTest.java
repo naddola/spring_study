c2b69e1b1af64c7a3a91ebe19630b84471b6156b
hcjeon <hcjeon@woomsoft.com>
2023-01-08 15:09:23 +0900

    2nd commit

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,33 @@
+package hello.hellospring.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
